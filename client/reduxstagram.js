2d7e94863338ed139c24b3d5c44bba1e37ba48c6
Cody McNamara <codyjamesmcnamara@gmail.com>
2016-05-22 17:16:05 -0700

    inital setup

---
+++ client/reduxstagram.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import { render } from 'react-dom';
+
+// import css
+import css from './styles/style.styl';
+
+// import Componenets
+import Main from './components/Main.js';
+
+render(<Main/>, document.getElementById('root'));
