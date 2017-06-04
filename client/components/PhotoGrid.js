846576e6e929fbc8e40ed170e4be21ce370edbc6
Cody McNamara <codyjamesmcnamara@gmail.com>
2017-06-04 13:35:10 -0700

    create Single and Photogrid components

---
+++ client/components/PhotoGrid.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const PhotoGrid = React.createClass({
+  render() {
+    return (
+      <div classname="photo-grid">
+        Im the photo grid
+      </div>
+    )
+  }
+});
+
+export default PhotoGrid;
