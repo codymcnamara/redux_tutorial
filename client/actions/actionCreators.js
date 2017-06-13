1a123289b4343f37367f0ce1436d54b909096455
Cody McNamara <codyjamesmcnamara@gmail.com>
2017-06-12 20:33:45 -0700

    add commenting and liking post reducers

--- client/actions/actionCreators.js
+++ client/actions/actionCreators.js
@@ -8,6 +8,7 @@ export function increment(index){
 
 // add comment
 export function addComment(postId, author, comment){
+  console.log('despatching add Comment')
   return {
     type: "ADD_COMMENT",
     postId,
@@ -19,6 +20,7 @@ export function addComment(postId, author, comment){
 // remove comment
 export function removeComment(postId, i){
   return{
+    type: "REMOVE_COMMENT",
     postId,
     i
   }
